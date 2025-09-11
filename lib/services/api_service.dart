c4abd32bdfeb106afe03fd692bc683d77b33fb5e
Callvcal <tech.callvcal@gmail.com>
2025-09-11 15:45:09 +0530

    createdParties

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -175,19 +175,19 @@ class ApiService {
       print(body);
       if (res.statusCode == 200) {
         HiveService.instance.updateUser(body['user']);
-        var createdParties = (body['createdParties'] as List).map(
-          (e) => e['name'],
-        );
-        HiveService.instance.put(
-          'createdParties',
-          createdParties.isEmpty
-              ? ["Vinod", "Digvijay", "Dilip", "Sampat", "Nanduji"]
-              : createdParties,
-        );
+
         HiveService.instance.updateSettings(body['settings']);
 
         HiveService.instance.putDashboardData(body);
-
+        var creditParties = (body['creditParties'] as List)
+            .map((e) => e['name'])
+            .toList();
+        HiveService.instance.put(
+          'creditParties',
+          creditParties.isEmpty
+              ? ["Vinod", "Digvijay", "Dilip", "Sampat", "Nanduji"]
+              : creditParties,
+        );
         // showSnackBar("Data Refreshed");
       } else {
         showSnackBar(body['message']);
