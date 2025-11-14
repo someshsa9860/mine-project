66bf1bb081b14867826c72140f413c82e1d1656f
Callvcal <tech.callvcal@gmail.com>
2025-11-14 08:38:45 +0530

    ssb.shrisahadevbajri.com on 14 Nov 2025

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
-const version = '1.0.13@11Sept25';
-const versionCode = 4;
+const version = '1.0.14@14Nov25';
+const versionCode = 5;
 const appName = "SSB";
 
 const creditParties = ["Vinod", "Digvijay", "Dilip", "Sampat", "Nanduji"];
