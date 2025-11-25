c08b409f477c0c4a0af6f3f13f1dece924eef94b
Callvcal <tech.callvcal@gmail.com>
2025-11-25 19:29:25 +0530

    2.0.0+15

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 const version = '2.0.0@15Nov25';
 const versionCode = 4;
-const appName = "SSB";
+const appName = "SSB-Jodhpur";
 
 const creditParties = ["Vinod", "Digvijay", "Dilip", "Sampat", "Nanduji"];
